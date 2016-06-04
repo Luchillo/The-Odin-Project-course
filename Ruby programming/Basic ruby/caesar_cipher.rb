b59fed35692b761789312d91266cf9d8cbe9e0ab
Luchillo <luchillo17@gmail.com>
2016-06-04 17:52:54 -0500

    Caesar cipher complete

--- Ruby programming/Basic ruby/caesar_cipher.rb
+++ Ruby programming/Basic ruby/caesar_cipher.rb
@@ -1,3 +1,21 @@
 def caesar_cipher(str, offset)
+   str = str.chars.map do |c|
+    next c unless /[A-Za-z]/ =~ c
 
+    case c
+    when ('a'..'z')
+      sum = 'a'.ord
+    when ('A'..'Z')
+      sum = 'A'.ord
+    else
+      sum = 0
+    end
+
+    ((c.ord - sum + offset) % ('z'.ord - 'a'.ord + 1) + sum).chr
+  end
+  puts str.join
+  str.join
 end
+
+caesar_cipher("What a string!", 5)
+puts "Bmfy f xywnsl!"
