622ce6ea85313ffe468b99c2e223c063dec6f94e
Luchillo <luchillo17@gmail.com>
2016-06-05 13:58:14 -0500

    Finished substrings

---
+++ Ruby-programming/Basic-ruby/substrings.rb
@@ -0,0 +1,19 @@
+def substrings str, dictionary
+  str_arr = str.split
+  hystogram = {}
+  str_arr.each do |word|
+    dictionary.each do |substr|
+      if word.downcase.include? substr.downcase
+        hystogram[substr] ||= 0
+        hystogram[substr] += 1
+      end
+    end
+  end
+  puts hystogram
+  hystogram
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => {"down"=>1, "how"=>2, "howdy"=>1,"go"=>1, "going"=>1, "it"=>2, "i"=> 3, "own"=>1,"part"=>1,"partner"=>1,"sit"=>1}
